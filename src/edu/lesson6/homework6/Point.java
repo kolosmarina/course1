d1a725a5c24a2d4b1d2a7d5cfdfd877159811ea8
Kolos Marina <kolosmarina3105@gmail.com>
2018-07-21 14:58:12 +0300

    Homework for lesson 6. Status: done.

---
+++ src/edu/lesson6/homework6/Point.java
@@ -0,0 +1,32 @@
+package edu.lesson6.homework6;
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt(Math.pow(x - point.getX(), 2) + Math.pow(y - point.getY(), 2));
+    }
+}
